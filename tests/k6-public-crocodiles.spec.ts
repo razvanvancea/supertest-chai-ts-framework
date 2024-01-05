472e0880077ed64739187e700cb17101f0c8c4d2
Razvan Vancea <razvan.vancea@zitec.com>
2024-01-05 23:36:02 +0200

    add more tests

--- tests/k6-public-crocodiles.spec.ts
+++ tests/k6-public-crocodiles.spec.ts
@@ -5,26 +5,28 @@ expect.extend(matchers);
 
 describe("public Crocodiles", () => {
   describe("GET crocodiles", () => {
-
     it("GET crocodiles", async () => {
       const res = await publicCrocodilesController.getCrocodiles();
       expect(res.statusCode).toEqual(200);
       expect(res.body.length).toBeGreaterThan(1);
       expect(Object.keys(res.body[0])).toEqual([
-       
         "id",
         "name",
         "sex",
         "date_of_birth",
         "age",
-
       ]);
     });
 
     it("GET crocodile by ID", async () => {
-        const res = await publicCrocodilesController.getCrocodileById(1);
-        expect(res.statusCode).toEqual(200);
-        expect(res.body).toMatchSchema(getCrocodileByIdSchema)
+      const res = await publicCrocodilesController.getCrocodileById(1);
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toMatchSchema(getCrocodileByIdSchema);
+    });
+
+    it("GET crocodile by invalid ID", async () => {
+        const res = await publicCrocodilesController.getCrocodileById(111111111111);
+        expect(res.statusCode).toEqual(404);
       });
   });
 });
